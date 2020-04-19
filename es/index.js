829e73b21f291e02159dd8b3d604ed3fc36b3280
jczzq <1195262383@qq.com>
2020-04-19 17:57:21 +0800

    chore: es&lib

---
+++ es/index.js
@@ -0,0 +1,29 @@
+import Button from './button';
+import Icon from './icon';
+import Info from './info';
+import Lazyload from './lazyload';
+import Loading from './loading';
+import Locale from './locale';
+import View from './view';
+var version = '0.0.1';
+
+function install(Vue) {
+  var components = [Button, Icon, Info, Loading, Locale, View];
+  components.forEach(function (item) {
+    if (item.install) {
+      Vue.use(item);
+    } else if (item.name) {
+      Vue.component(item.name, item);
+    }
+  });
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export { install, version, Button, Icon, Info, Lazyload, Loading, Locale, View };
+export default {
+  install: install,
+  version: version
+};
---
+++ es/locale/lang/zh-CN.js
@@ -0,0 +1,79 @@
+export default {
+  name: '姓名',
+  tel: '电话',
+  save: '保存',
+  confirm: '确认',
+  cancel: '取消',
+  delete: '删除',
+  complete: '完成',
+  loading: '加载中...',
+  moneCalendar: {
+    end: '结束',
+    start: '开始',
+    title: '日期选择',
+    confirm: '确定',
+    weekdays: ['日', '一', '二', '三', '四', '五', '六'],
+    monthTitle: function monthTitle(year, month) {
+      return year + "\u5E74" + month + "\u6708";
+    }
+  },
+  moneContactCard: {
+    addText: '添加联系人'
+  },
+  moneContactList: {
+    addText: '新建联系人'
+  },
+  monePagination: {
+    prev: '上一页',
+    next: '下一页'
+  },
+  monePullRefresh: {
+    pulling: '下拉即可刷新...',
+    loosing: '释放即可刷新...'
+  },
+  moneSubmitBar: {
+    label: '合计：'
+  },
+  moneCoupon: {
+    unlimited: '无使用门槛',
+    discount: function discount(_discount) {
+      return _discount + "\u6298";
+    },
+    condition: function condition(_condition) {
+      return "\u6EE1" + _condition + "\u5143\u53EF\u7528";
+    }
+  },
+  moneCouponCell: {
+    title: '优惠券',
+    tips: '使用优惠',
+    count: function count(_count) {
+      return _count + "\u5F20\u53EF\u7528";
+    }
+  },
+  moneCouponList: {
+    empty: '暂无优惠券',
+    exchange: '兑换',
+    close: '不使用优惠券',
+    enable: '可用',
+    disabled: '不可用',
+    placeholder: '请输入优惠码'
+  },
+  moneAddressEdit: {
+    area: '地区',
+    postal: '邮政编码',
+    areaEmpty: '请选择地区',
+    addressEmpty: '请填写详细地址',
+    postalEmpty: '邮政编码格式不正确',
+    defaultAddress: '设为默认收货地址',
+    telPlaceholder: '收货人手机号',
+    namePlaceholder: '收货人姓名',
+    areaPlaceholder: '选择省 / 市 / 区'
+  },
+  moneAddressEditDetail: {
+    label: '详细地址',
+    placeholder: '街道门牌、楼层房间号等信息'
+  },
+  moneAddressList: {
+    add: '新增地址'
+  }
+};
